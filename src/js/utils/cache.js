7ec7e70352f36f41bd8ca00a8d6fe06c937db6e8
Gianmarco Brunialti Masera <49029157+justmerge@users.noreply.github.com>
2020-08-07 12:48:18 +0200

    DD-15 Cache also cards back-face image

--- src/js/utils/cache.js
+++ src/js/utils/cache.js
@@ -3,6 +3,10 @@ import { EVENTS } from 'constants';
 
 const { CARD_READY } = EVENTS;
 
+const BACK_FACE_IMAGE_URL = 'https://static.vecteezy.com/' +
+    'system/resources/previews/000/396/130/large_2x/' +
+    'vector-minimal-black-and-gold-geometric-pattern.jpg';
+
 let cachedCards = 0;
 
 function notifyCardReady(deckSize) {
@@ -16,12 +20,13 @@ function notifyCardReady(deckSize) {
 }
 
 export function cacheDeck(deck) {
-    const deckSize = deck.length;
+    const fullDeck = [...deck, { image: BACK_FACE_IMAGE_URL }],
+        fullDeckSize = fullDeck.length;
 
-    deck.forEach(card => {
+    fullDeck.forEach(card => {
         const cardImage = w.document.createElement('img');
 
-        cardImage.onload = () => notifyCardReady(deckSize);
+        cardImage.onload = () => notifyCardReady(fullDeckSize);
         cardImage.src = card.image;
     });
 }
